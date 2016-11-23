e7a9d5b8909d887c8ea4b27ff2d29f20d1070dc4
xingguoye <xingguoye@gmail.com>
2016-11-23 18:20:45 +0100

    Prueba 3

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hola.");
+        Objeto1 objeto1 = new Objeto1();
     }
 }
---
+++ src/Objeto1.java
@@ -0,0 +1,5 @@
+/**
+ * Created by movilin on 2016/11/23.
+ */
+public class Objeto1 {
+}
